d16296d6ee5059ef8fa37327dd20b3033e0228f2
MeansKane <phamminhkhangpy2003@gmail.com>
2024-05-31 16:22:04 +0700

    make use of worker enqueue the work request with workManger

--- app/src/main/java/com/example/workmanagerapp/MainActivity.java
+++ app/src/main/java/com/example/workmanagerapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.workmanagerapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btn = findViewById(R.id.runWM);
+
+        // making use of worker
+        WorkRequest wr = new OneTimeWorkRequest.Builder(Worker.class).build();
+
+        // enqueue the request with WorkManager
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WorkManager.getInstance(getApplicationContext()).enqueue(wr);
+            }
         });
     }
 }
