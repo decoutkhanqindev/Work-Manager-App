b800fdf138ffe7f44f8a961785f3dabd6d21f37f
MeansKane <phamminhkhangpy2003@gmail.com>
2024-05-31 16:06:58 +0700

    ...

---
+++ app/src/main/java/com/example/workmanagerapp/Worker.java
@@ -0,0 +1,22 @@
+package com.example.workmanagerapp;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.WorkerParameters;
+
+public class Worker extends androidx.work.Worker {
+    public Worker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        for (int i = 0; i < 1000; i++){
+            Log.e("TAGY", "Count is: " + i);
+        }
+        return Result.success();
+    }
+}
